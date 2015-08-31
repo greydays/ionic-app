2d7c2314c4a27486e83cdf133092b395545926af
Ryan Flaherty <rcflaherty@gmail.com>
2015-08-30 20:42:00 -0700

    calc for near field working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,15 +4,25 @@ angular.module('starter.controllers', [])
   $scope.parseFloat = function(value) {
     return parseFloat(value);
   };
-  $scope.toast = function(value) {
-    return value * 3;
-  }
+
+  $scope.data = {
+    cOfCOptions: [
+      {name:'0.011', val: 0.011},
+      {name:'0.015', val: 0.015},
+      {name:'0.018', val: 0.018},
+      {name:'0.019', val: 0.019},
+      {name:'0.020', val: 0.020},
+      {name:'0.030', val: 0.030}
+    ],
+  };
 
   $scope.nearDepth = function(cOfC, focal, aperture, distance) {
-    console.log(cOfC);
-    var hyperFocal = (parseFloat(focal) * parseFloat(focal)) / (parseFloat(aperture) * parseFloat(cOfC)) + parseFloat(focal);
-    console.log(hyperFocal);
-    return ((hyperFocal - focal) * distance) / (hyperFocal + distance - (2 * focal));
+    var focalVal = parseFloat(focal);
+    var apertureVal = parseFloat(aperture);
+    var cOfCVal = parseFloat(cOfC);
+    var distanceVal = parseFloat(distance) * 1000;
+    var hyperFocal = (focalVal * focalVal) / (apertureVal * cOfCVal) + focalVal;
+    return (((hyperFocal - focalVal) * distanceVal) / (hyperFocal + distanceVal - (2 * focalVal))) / 1000.0;
   }
 
 }])
@@ -42,8 +52,4 @@ angular.module('starter.controllers', [])
   };
 });
 
-// .controller('calcCtrl', function($scope) {
-//   $scope.parseFloat = function(value) {
-//     return parseFloat(value);
-//   };
-// });
+
